ed2795dd9e98af5ee79794d3c9b426b081bb18b4
Usama6270 <usamaunder19@gmail.com>
2025-04-16 20:59:39 +0500

    first commit

---
+++ Datatype.py
@@ -0,0 +1,48 @@
+#*
+print("Hello world")
+print (25+25)
+name="usama"
+age=23
+hobby="cricket"
+print(type(hobby))
+name="usama"
+print(name)
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
+#print sum
+a=2
+b=5
+sum=a+b
+print(a+b)
+a,b=2,3
+txt="@"
+print(a*txt*b)
+#closest integer //
+nm=input("nm: ")
+print(nm)
+nam=int(input("nam: "))
+print(nam)
+#conditional statement
+age=int(input("age : "))
+if(age>18):
+    print("vote accept")
+elif(age>16 and age<10):
+    print("not vote")
+else:
+    print("child") 
+#type casting
+a=("4") 
+b=int(a) 
+print(type(b))
+#input simple alway show an str
+#input("enter the name")
+#practice
+a=float(input("enter number 1"))
+b=float(input("enter number 2"))
+avg=(a+b)/2
+print(avg)
+#complete Datatype concept
+
