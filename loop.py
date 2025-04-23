3a8035d21e37fa71bd96c6d787c319c93114b67f
Usama6270 <usamaunder19@gmail.com>
2025-04-23 23:13:07 +0500

    6

---
+++ loop.py
@@ -0,0 +1,38 @@
+# #loops
+# i=1
+# while(i<5):
+#     print("hello")
+#     i=i+1
+# print("for loop")
+# for i in range(1,5):
+#     print("hello")
+# print("done looping")
+
+#practice question while loop
+
+# i=100
+# while(i>1):
+#     print(i)
+#     i=i-1
+
+# n=int(input("n: "))
+# i=1
+# while(i<=10):
+#     print(n*i)
+#     i+=1
+
+num=[1,2,3,4,5,44]
+indx=0
+while(indx<len(num)):
+    print(num[indx])
+    indx+=1
+
+#for loops
+num=[1,2,3,4,5,44]
+for i in num:
+    print(i)
+
+for i in range(1,10):
+    print(i)
+else:
+    print("done")
